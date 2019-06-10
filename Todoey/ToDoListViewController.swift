d792be77aca14c51b7f78cdc2ec64ab95b5b9d51
Gabor Papp <pggabo@gmail.com>
2019-06-10 19:50:03 +0200

    Táblázat elkészült

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Gabor Papp on 2019. 06. 10..
+//  Copyright © 2019. Gabor Papp. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK - Tableview datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray [indexPath.row]
+        
+        return cell
+    }
+
+}
+
